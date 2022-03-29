34e12109ee170cb029ba7ed2728c568b0bfbb62e
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-03-29 10:52:03 +0100

    Test passed - 2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 export class Main {
+  private cards = '123456789TJQK';
   constructor() {}
 
   public playerWins(player1: string[], player2: string[]): string {
-    if (parseInt(player1[0]) > parseInt(player2[0])) {
+    if (this.cards.indexOf(player1[0]) > this.cards.indexOf(player2[0])) {
       return 'Player 1 wins!';
     }
     return 'Player 2 wins!';
